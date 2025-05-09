9cd3fd053f5dbc92a01e07c2172d5b868cd57232
bezstrok <pocandre87@gmail.com>
2025-05-09 20:02:50 +0300

    Update index field in topic_profiles collection

--- scripts/initialization/create-indexes.js
+++ scripts/initialization/create-indexes.js
@@ -3,6 +3,6 @@ db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
 db.users.createIndex({ user_id: 1 }, { unique: true });
 
 db.topic_profiles.createIndex({ user_id: 1 }, { unique: true });
-db.topic_profiles.createIndex({ "topics.name": 1 });
+db.topic_profiles.createIndex({ "keywords.name": 1 });
 db.topic_profiles.createIndex({ "entities.category": 1 });
 db.topic_profiles.createIndex({ "entities.name": 1 });
