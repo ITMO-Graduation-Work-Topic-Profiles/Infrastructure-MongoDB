4d28df0dda14862e7d73e268385d3aff7f7fdad5
bezstrok <pocandre87@gmail.com>
2025-05-11 02:03:37 +0300

    Update topic_profiles indexes for nested attributes

--- scripts/initialization/create-indexes.js
+++ scripts/initialization/create-indexes.js
@@ -3,7 +3,7 @@ db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
 db.users.createIndex({ user_id: 1 }, { unique: true });
 
 db.topic_profiles.createIndex({ user_id: 1 }, { unique: true });
-db.topic_profiles.createIndex({ "keywords.name": 1 });
-db.topic_profiles.createIndex({ "entities.category": 1 });
-db.topic_profiles.createIndex({ "entities.name": 1 });
-db.topic_profiles.createIndex({ "sentiments.name": 1 });
+db.topic_profiles.createIndex({ "topic_attributes.keywords.name": 1 });
+db.topic_profiles.createIndex({ "topic_attributes.entities.category": 1 });
+db.topic_profiles.createIndex({ "topic_attributes.entities.name": 1 });
+db.topic_profiles.createIndex({ "topic_attributes.sentiments.name": 1 });
