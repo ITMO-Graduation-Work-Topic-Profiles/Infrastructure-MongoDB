1d844417cb4c847dc125f6cd009c959a0cf25796
bezstrok <pocandre87@gmail.com>
2025-05-12 18:45:11 +0300

    Rename 'topic_attributes' collection to 'aggregated_topic_attributes'.

--- scripts/initialization/create-collections.js
+++ scripts/initialization/create-collections.js
@@ -1,5 +1,5 @@
 db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
 
 db.createCollection("users");
-db.createCollection("topic_attributes");
+db.createCollection("aggregated_topic_attributes");
 db.createCollection("topic_profiles");
--- scripts/initialization/create-indexes.js
+++ scripts/initialization/create-indexes.js
@@ -2,8 +2,8 @@ db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
 
 db.users.createIndex({ user_id: 1 }, { unique: true });
 
-db.topic_attributes.createIndex({ user_id: 1 }, { unique: true });
-db.topic_attributes.createIndex({ "keywords.name": 1 });
-db.topic_attributes.createIndex({ "entities.category": 1 });
-db.topic_attributes.createIndex({ "entities.name": 1 });
-db.topic_attributes.createIndex({ "sentiments.name": 1 });
+db.aggregated_topic_attributes.createIndex({ user_id: 1 }, { unique: true });
+db.aggregated_topic_attributes.createIndex({ "keywords.name": 1 });
+db.aggregated_topic_attributes.createIndex({ "entities.category": 1 });
+db.aggregated_topic_attributes.createIndex({ "entities.name": 1 });
+db.aggregated_topic_attributes.createIndex({ "sentiments.name": 1 });
