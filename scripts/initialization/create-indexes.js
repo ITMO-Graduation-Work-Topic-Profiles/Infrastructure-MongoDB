a021482a54264526574f082c3ed4dbb043f55f56
bezstrok <pocandre87@gmail.com>
2025-05-13 07:33:40 +0300

    Add indexes for topic_profiles collection.

--- scripts/initialization/create-indexes.js
+++ scripts/initialization/create-indexes.js
@@ -7,3 +7,7 @@ db.aggregated_topic_attributes.createIndex({ "keywords.name": 1 });
 db.aggregated_topic_attributes.createIndex({ "entities.category": 1 });
 db.aggregated_topic_attributes.createIndex({ "entities.name": 1 });
 db.aggregated_topic_attributes.createIndex({ "sentiments.name": 1 });
+
+db.topic_profiles.createIndex({ user_id: 1 }, { unique: true });
+db.topic_profiles.createIndex({ "topics.labels.name": 1 });
+db.topic_profiles.createIndex({ "topics.words.name": 1 });
